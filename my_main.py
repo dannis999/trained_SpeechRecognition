bdc98de62e5e466a77601126c13f99208471d62c
dannis999 <39402843+dannis999@users.noreply.github.com>
2022-02-05 09:34:40 +0000

    more robust under big files

--- my_main.py
+++ my_main.py
@@ -1,4 +1,4 @@
-import os,pydub
+import os,pydub,gc
 import numpy as np
 import scipy.interpolate as sint
 from speech_model import ModelSpeech
@@ -17,43 +17,55 @@ feat = Spectrogram()
 ms = ModelSpeech(sm251, feat, max_label_length=64)
 ms.load_model(os.path.join('save_models','SpeechModel251.model.h5'))
 
-def load_wav(fn,glb_fs=16000):
+def traverse_wav(fn,glb_fs=16000,sep_sec=16,overlap_sec=1):
     print('正在读取音频文件')
     _,suf = fn.rsplit('.',1)
     suf = suf.lower()
     func = getattr(pydub.AudioSegment,f'from_{suf}')
     d = func(fn)
+    del func
+    print('正在转换数据')
     fs = d.frame_rate
-    wave = d.get_array_of_samples()
-    wave = np.array(wave)
-    if d.channels > 1:
+    b = d.raw_data
+    channels = d.channels
+    array_type = d.array_type
+    del d
+    gc.collect()
+    wave = np.frombuffer(b,dtype=array_type)
+    del b
+    gc.collect()
+    if channels > 1:
         print('正在合并声道')
-        wave = wave.reshape((-1,d.channels))
-        wave = np.mean(wave,axis=1)
-    if fs != glb_fs:
-        print('正在重采样')
-        n1 = len(wave)
-        n2 = round(n1 * glb_fs / fs)
-        x1 = np.linspace(0,1,n1)
-        x2 = np.linspace(0,1,n2)
-        wave = sint.interp1d(x1,wave,kind='cubic')(x2)
-    wave = wave.astype(np.short,copy=False)
-    return wave, glb_fs
-
-def recog_file(fn_wav:str,fn_pinyin:str,sep_sec=16,overlap_sec=1):
-    wave,fs = load_wav(fn_wav)
+        wave = wave.reshape((-1,channels))
+        wave = np.mean(wave,axis=1,dtype=wave.dtype)
     print('正在识别')
     n = len(wave)
     sep = round(sep_sec * fs)
     overlap = round(overlap_sec * fs)
     step = sep - overlap
+    if fs != glb_fs:
+        n1 = sep
+        n2 = round(n1 * glb_fs / fs)
+        x1 = np.linspace(0,1,n1)
+        x2 = np.linspace(0,1,n2)
     i = 0
+    while 1:
+        dn = min(i+sep,n)
+        dwave = wave[i:dn]
+        if fs != glb_fs:
+            dx1 = x1[:len(dwave)]
+            dx2 = x2[x2 <= dx1[-1]]
+            dwave = sint.interp1d(dx1,dwave,kind='cubic')(dx2)
+        dwave = dwave.astype(np.short,copy=False)
+        sec_start = round(i / fs)
+        yield dwave,glb_fs,sec_start
+        if dn >= n:break
+        i += step
+
+def recog_file(fn_wav:str,fn_pinyin:str,sep_sec=16,overlap_sec=1):
     with open(fn_pinyin,'w',encoding='utf-8') as f:
-        while 1:
-            dn = min(i+sep,n)
-            dwave = wave[i:dn]
-            dr = ms.recognize_speech(dwave[None,:], fs)
-            sec_start = round(i / fs)
+        for wave,fs,sec_start in traverse_wav(fn_wav):
+            dr = ms.recognize_speech(wave[None,:], fs)
             if dr:
                 m,s = divmod(sec_start,60)
                 h,m = divmod(m,60)
@@ -61,8 +73,6 @@ def recog_file(fn_wav:str,fn_pinyin:str,sep_sec=16,overlap_sec=1):
                 ps = ' '.join(dr)
                 f.write(f'[{ts}]{ps}\n')
                 f.flush()
-            if dn >= n:break
-            i += step
 
 def main():
     fn1 = input()
