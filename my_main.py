b3318ea0da9a3198df674d462d6e8de97aa4aab2
dannis999 <39402843+dannis999@users.noreply.github.com>
2022-02-05 08:38:16 +0000

    add tips

--- my_main.py
+++ my_main.py
@@ -18,6 +18,7 @@ ms = ModelSpeech(sm251, feat, max_label_length=64)
 ms.load_model(os.path.join('save_models','SpeechModel251.model.h5'))
 
 def load_wav(fn,glb_fs=16000):
+    print('正在读取音频文件')
     _,suf = fn.rsplit('.',1)
     suf = suf.lower()
     func = getattr(pydub.AudioSegment,f'from_{suf}')
@@ -26,9 +27,11 @@ def load_wav(fn,glb_fs=16000):
     wave = d.get_array_of_samples()
     wave = np.array(wave)
     if d.channels > 1:
+        print('正在合并声道')
         wave = wave.reshape((-1,d.channels))
         wave = np.mean(wave,axis=1)
     if fs != glb_fs:
+        print('正在重采样')
         n1 = len(wave)
         n2 = round(n1 * glb_fs / fs)
         x1 = np.linspace(0,1,n1)
@@ -39,24 +42,27 @@ def load_wav(fn,glb_fs=16000):
 
 def recog_file(fn_wav:str,fn_pinyin:str,sep_sec=16,overlap_sec=1):
     wave,fs = load_wav(fn_wav)
+    print('正在识别')
     n = len(wave)
     sep = round(sep_sec * fs)
     overlap = round(overlap_sec * fs)
     step = sep - overlap
     i = 0
     with open(fn_pinyin,'w',encoding='utf-8') as f:
-        while i < n:
+        while 1:
             dn = min(i+sep,n)
             dwave = wave[i:dn]
             dr = ms.recognize_speech(dwave[None,:], fs)
             sec_start = round(i / fs)
-            i = dn
-            if not dr:continue
-            m,s = divmod(sec_start,60)
-            h,m = divmod(m,60)
-            ts = '{0:02}:{1:02}:{2:02}'.format(h,m,s)
-            ps = ' '.join(dr)
-            f.write(f'[{ts}]{ps}\n')
+            if dr:
+                m,s = divmod(sec_start,60)
+                h,m = divmod(m,60)
+                ts = '{0:02}:{1:02}:{2:02}'.format(h,m,s)
+                ps = ' '.join(dr)
+                f.write(f'[{ts}]{ps}\n')
+                f.flush()
+            if dn >= n:break
+            i += step
 
 def main():
     fn1 = input()
